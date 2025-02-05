945fce9af8a042627a1a30cc4996386aedce0050
AnatoliiM18 <mamaev-av18@ya.ru>
2025-02-05 23:13:51 +0400

    Add logger files and include EASTL

---
+++ src/logger.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstdint>
+#include <EASTL/map.h>
+
+/* Logger whrite flags to file or debugger */
+enum LogFlag{
+    LOGFLAG_WRT_TO_FILE = 1,
+    LOGFLAG_WRT_TO_DBGR = 2,
+};
+
+namespace logger
+{
+
+}
